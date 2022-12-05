c1a567446be2ae3cbc9b8d3486dfc688e0ca6a3c
Shameera <shameera@pop-os.localdomain>
2022-12-04 19:02:39 -0800

    Day 03 code

--- main.go
+++ main.go
@@ -10,10 +10,24 @@ func main() {
   // fmt.Printf("Day01_02: Sum of top 3 max Calary sums: %d\n", Day01_SecondAnswer())
 
 
-  day02Input := ReadLines("inputs/day02_input.txt")
+  // day02Input := ReadLines("inputs/day02_input.txt")
   // fmt.Printf("Day02_01: Total Score: %d\n", Day02_FirstAnswer(day02Input))
   // day02Input := []string{"A Y", "B X", "C Z"} 
-  fmt.Printf("Day02_02: Total Score: %d\n", Day02_SecondAnswer(day02Input))
+  // fmt.Printf("Day02_02: Total Score: %d\n", Day02_SecondAnswer(day02Input))
+
+  
+  day03Input := ReadLines("inputs/day03_input.txt")
+  // day03Input := []string{
+  //   "vJrwpWtwJgWrhcsFMMfFFhFp",
+  //   "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+  //   "PmmdzqPrVvPwwTWBwg",
+  //   "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
+  //   "ttgJtRGJQctTZtZT",
+  //   "CrZsJsPPZsGzwwsLwLmpwMDw",
+  // } 
+
+  // fmt.Printf("Day03_01: Total Score: %d\n", Day03_FirstAnswer(day03Input))
+  fmt.Printf("Day03_02: Total Score: %d\n", Day03_SecondAnswer(day03Input))
 
 }
 
